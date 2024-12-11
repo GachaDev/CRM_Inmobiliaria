821ba90c58ce0e4134cb8d6fcc0d02e260c79ef3
Gacha <jlgacha75@gmail.com>
2024-12-11 12:00:49 +0100

    Init

---
+++ src/main/java/com/es/crmInmobiliaria/CrmInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.es.crmInmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmInmobiliariaApplication.class, args);
+	}
+
+}
